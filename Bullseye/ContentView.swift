cb7af48d03e9108be809012d98fc70b3fabca5ad
James Elder <james@elder-software.com>
2021-09-05 13:15:42 +1000

    Bullseye view progress

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            VStack {
+                Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO ")
+                    .bold()
+                    .kerning(2.0)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4.0)
+                    .font(.footnote)
+                Text("89")
+                HStack {
+                    Text("1")
+                    Slider(value: .constant(50), in: 1.0...100)
+                    Text("100")
+                }
+            }
+            Button(action: {}) {
+                Text("Hit me")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
+        ContentView()
+            .previewLayout(.fixed(width: 560, height: 320))
     }
 }
