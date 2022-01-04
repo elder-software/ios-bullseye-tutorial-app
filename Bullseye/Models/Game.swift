329d0440b322a660eee17e9de807b65608e9612e
James Elder <james@elder-software.com>
2022-01-05 09:19:26 +1100

    Added the game model and connected to the view

---
+++ Bullseye/Models/Game.swift
@@ -0,0 +1,18 @@
+//
+//  Game.swift
+//  Bullseye
+//
+//  Created by James Elder on 5/01/22.
+//
+
+import Foundation
+
+struct Game {
+  var target: Int = 42
+  var score: Int = 0
+  var round: Int = 1
+  
+  func points(sliderValue: Int) -> Int {
+    return 999
+  }
+}
--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -8,60 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var alertIsVisible: Bool = false
-    @State private var sliderValue: Double = 50.0
-
-    var body: some View {
+  @State private var alertIsVisible: Bool = false
+  @State private var sliderValue: Double = 50.0
+  @State private var game: Game = Game()
+  
+  var body: some View {
+    VStack {
+      VStack {
         VStack {
-            VStack {
-                VStack {
-                    Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO ")
-                        .bold()
-                        .kerning(2.0)
-                        .multilineTextAlignment(.center)
-                        .lineSpacing(4.0)
-                        .font(.footnote)
-                    Text("89")
-                        .kerning(-1.0)
-                        .font(.largeTitle)
-                        .fontWeight(.black)
-                    HStack {
-                        Text("1")
-                            .bold()
-                            .font(.body)
-                            .padding()
-                        Slider(value: self.$sliderValue, in: 1.0...100)
-                            .accentColor(.red)
-                        Text("100")
-                            .bold()
-                            .font(.body)
-                            .padding()
-                    }
-                }
-                VStack {
-                    Button(action: {
-                        self.alertIsVisible = true
-                    }) {
-                        Text("Hit me")
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(Color.accentColor)
-                    }
-                    .alert(isPresented: $alertIsVisible, content: {
-                        let roundedValue: Int = Int(self.sliderValue.rounded())
-                        return Alert(title: Text("Hello"), message: Text("The sliders value is \(roundedValue)."), dismissButton: .default(Text("Dismiss")))
-                    })
-                }
-            }
+          Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO ")
+            .bold()
+            .kerning(2.0)
+            .multilineTextAlignment(.center)
+            .lineSpacing(4.0)
+            .font(.footnote)
+          Text(String(game.target))
+            .kerning(-1.0)
+            .font(.largeTitle)
+            .fontWeight(.black)
+          HStack {
+            Text("1")
+              .bold()
+              .font(.body)
+              .padding()
+            Slider(value: self.$sliderValue, in: 1.0...100)
+              .accentColor(.red)
+            Text("100")
+              .bold()
+              .font(.body)
+              .padding()
+          }
         }
+        VStack {
+          Button(action: {
+            self.alertIsVisible = true
+          }) {
+            Text("Hit me")
+              .foregroundColor(.white)
+              .padding()
+              .background(Color.accentColor)
+          }
+          .alert(isPresented: $alertIsVisible, content: {
+            let roundedValue: Int = Int(self.sliderValue.rounded())
+            return Alert(title: Text("Hello"), message: Text("The sliders value is \(roundedValue).\n" + "You scored \(self.game.points(sliderValue: roundedValue)) points this round"), dismissButton: .default(Text("Dismiss")))
+          })
+        }
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            
-        ContentView()
-            .previewLayout(.fixed(width: 560, height: 320))
-    }
+  static var previews: some View {
+    ContentView()
+    
+    ContentView()
+      .previewLayout(.fixed(width: 560, height: 320))
+  }
 }
