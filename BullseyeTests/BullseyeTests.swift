1f30e897f52e57e8b5bb7a00681c1c2e51403fec
James Elder <james@elder-software.com>
2022-01-18 13:28:47 +1100

    added tests and points method

--- BullseyeTests/BullseyeTests.swift
+++ BullseyeTests/BullseyeTests.swift
@@ -2,32 +2,35 @@
 //  BullseyeTests.swift
 //  BullseyeTests
 //
-//  Created by James Elder on 5/09/21.
+//  Created by James Elder on 18/01/22.
 //
 
 import XCTest
 @testable import Bullseye
 
 class BullseyeTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+  
+  var game: Game!
+  
+  override func setUpWithError() throws {
+    game = Game()
+  }
+  
+  override func tearDownWithError() throws {
+    game = nil
+  }
+  
+  func testScorePositive() {
+    var guess = game.target + 5
+    var score = game.points(sliderValue: guess)
+    
+    XCTAssertEqual(score, 95)
+  }
+  
+  func testScoreNegative() {
+    var guess = game.target - 5
+    var score = game.points(sliderValue: guess)
+    
+    XCTAssertEqual(score, 95)
+  }
 }
