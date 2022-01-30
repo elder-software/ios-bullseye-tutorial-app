bc909faf1426d970de7ad9a4d24134581dcc5e54
James Elder <james@elder-software.com>
2022-01-31 09:18:39 +1100

    Added assets and nicer UI

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -13,45 +13,56 @@ struct ContentView: View {
   @State private var game = Game()
   
   var body: some View {
-    VStack {
+    ZStack {
+      Color("BackgroundColour")
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
       VStack {
         VStack {
-          Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO ")
-            .bold()
-            .kerning(2.0)
-            .multilineTextAlignment(.center)
-            .lineSpacing(4.0)
-            .font(.footnote)
-          Text(String(game.target))
-            .kerning(-1.0)
-            .font(.largeTitle)
-            .fontWeight(.black)
-          HStack {
-            Text("1")
+          VStack {
+            Text("🎯🎯🎯\nPut the Bullseye as close as you can to ".uppercased())
               .bold()
-              .font(.body)
-              .padding()
-            Slider(value: $sliderValue, in: 1.0...100)
-              .accentColor(.red)
-            Text("100")
-              .bold()
-              .font(.body)
-              .padding()
+              .kerning(2.0)
+              .multilineTextAlignment(.center)
+              .lineSpacing(4.0)
+              .font(.footnote)
+            Text(String(game.target))
+              .kerning(-1.0)
+              .font(.largeTitle)
+              .fontWeight(.black)
+            HStack {
+              Text("1")
+                .bold()
+                .font(.body)
+              Slider(value: $sliderValue, in: 1.0...100)
+                .accentColor(.red)
+              Text("100")
+                .bold()
+                .font(.body)
+            }
+            .padding()
           }
-        }
-        VStack {
-          Button(action: {
-            alertIsVisible = true
-          }) {
-            Text("Hit me")
-              .foregroundColor(.white)
-              .padding()
-              .background(Color.accentColor)
+          VStack {
+            Button(action: {
+              alertIsVisible = true
+            }) {
+              Text("Hit me".uppercased())
+                .bold()
+                .font(.title3)
+            }
+            .padding(20.0)
+            .background(
+              ZStack {
+                Color("ButtonColour")
+                LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+              }
+            )
+            .foregroundColor(Color.white)
+            .cornerRadius(21.0)
+            .alert(isPresented: $alertIsVisible, content: {
+              let roundedValue = Int(sliderValue.rounded())
+              return Alert(title: Text("Hello"), message: Text("The sliders value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round"), dismissButton: .default(Text("Dismiss")))
+            })
           }
-          .alert(isPresented: $alertIsVisible, content: {
-            let roundedValue = Int(sliderValue.rounded())
-            return Alert(title: Text("Hello"), message: Text("The sliders value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round"), dismissButton: .default(Text("Dismiss")))
-          })
         }
       }
     }
@@ -61,8 +72,5 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
-    
-    ContentView()
-      .previewLayout(.fixed(width: 560, height: 320))
   }
 }
